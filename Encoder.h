01cd6e5bfd051b72df91fcfa1b694298feeead3f
Yoshiki Oka <urandq@gmail.com>
2023-02-19 14:32:22 +0900

    add Qei, refactor Encoder

--- Encoder.h
+++ Encoder.h
@@ -5,14 +5,11 @@
 #include <mstd_atomic>
  
 namespace mext {
- 
-template <class T = int>
-class Encoder {
-public:
-	static_assert(std::is_integral<T>::value, "T must be integral");
 
-	explicit Encoder(PinName pin1, PinName pin2)
-		: accumulated_ticks_(), revert_(false)
+class Qei {
+public:
+	explicit Qei(PinName pin1, PinName pin2)
+		: revert_(false)
 	{
 		if (mhal::encoder_pin_polarity(pin1, pin2) == mhal::encoder_pin_polarity_rev) {
 			mhal::encoder_init(&enc_, pin2, pin1);
@@ -21,43 +18,10 @@ public:
 			mhal::encoder_init(&enc_, pin1, pin2);
 		}
 		resol_ = mhal::encoder_counter_resolution(&enc_);
-		reset_counter();
+		write_zero();
 	}
 
 	int counter() const {
-		return static_cast<int>(read_from_counter());
-	}
- 
-	T accumulated_ticks() const {
-		return accumulated_ticks_.load(mstd::memory_order_relaxed);
-	}
-
-	T ticks() const {
-		CriticalSectionLock lock;
-		return counter() + accumulated_ticks();
-	}
- 
-	void accumulate() {
-		CriticalSectionLock lock;
- 
-		accumulated_ticks_.fetch_add(read_from_counter(), mstd::memory_order_relaxed);
-		reset_counter();
-	}
- 
-	void reset() {
-		CriticalSectionLock lock;
-
-		accumulated_ticks_.store(0, mstd::memory_order_relaxed);
-		reset_counter();
-	}
-	
-	~Encoder() {
-		mhal::encoder_free(&enc_);
-	}
- 
-private:
- 
-	std::int32_t read_from_counter() const {
 
 		std::int32_t cnt;
 
@@ -71,20 +35,60 @@ private:
 			cnt = -cnt;
 		}
 
-		return cnt;
+		return static_cast<int>(cnt);
 	}
- 
-	void reset_counter() {
+
+	void write_zero() {
 		mhal::encoder_write(&enc_, 0);
 	}
+	
+	~Qei() {
+		mhal::encoder_free(&enc_);
+	}
 
+private:
 	DeepSleepLock lock;
-
 	mhal::encoder_t enc_;
- 
-	mstd::atomic<T> accumulated_ticks_; // ISR atomicity required here because the read has to be atomic
 	mhal::encoder_counter_resolution_t resol_;
 	bool revert_;
+};
+ 
+template <class T = int>
+class Encoder {
+public:
+	static_assert(std::is_integral<T>::value, "T must be integral");
+
+	explicit Encoder(PinName pin1, PinName pin2)
+		: accumulated_ticks_(0), qei_(pin1, pin2)
+	{
+	}
+
+	int counter() const {
+		return qei_.counter();
+	}
+ 
+	T ticks() const {
+		CriticalSectionLock lock;
+		return qei_.counter() + accumulated_ticks_;
+	}
+ 
+	void accumulate() {
+		CriticalSectionLock lock;
+ 
+		accumulated_ticks_ += qei_.counter();
+		qei_.write_zero();
+	}
+ 
+	void reset() {
+		CriticalSectionLock lock;
+
+		accumulated_ticks_ = 0;
+		qei_.write_zero();
+	}
+ 
+private:
+	T accumulated_ticks_;
+	Qei qei_;
 };
  
 }
